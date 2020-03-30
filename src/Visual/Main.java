b13412eb9a68383910087601b816ead270d75bbe
ze@home <ze@home>
2020-03-30 20:25:10 -0300

    com texto colorido em aba code2

--- src/Visual/Main.java
+++ src/Visual/Main.java
@@ -9,33 +9,45 @@ import Processing.DataProcessor;
 import Responses.RunResponse;
 import File.File;
 import File.FileControler;
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.Image;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
+import javax.swing.text.Highlighter.HighlightPainter;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 /**
  *
  * @author Rafhael
  */
 public class Main extends javax.swing.JFrame {
+
     File file = null;
     FileControler Freader = new FileControler();
     DataProcessor Dprocessor = new DataProcessor();
+
     @Override
     public void setIconImage(Image image) {
         super.setIconImage(image); //To change body of generated methods, choose Tools | Templates.
     }
+
     /**
      * Creates new form mai
      */
     public Main() {
-        
-        
+
         initComponents();
-        
+
     }
 
     /**
@@ -56,6 +68,8 @@ public class Main extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         AIndexArea = new javax.swing.JTextArea();
+        jScrollPane5 = new javax.swing.JScrollPane();
+        textPaneTeste = new javax.swing.JTextPane();
         jScrollPane3 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
@@ -64,6 +78,7 @@ public class Main extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
@@ -92,7 +107,7 @@ public class Main extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 375, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 438, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -110,7 +125,7 @@ public class Main extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 375, Short.MAX_VALUE)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 438, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -118,6 +133,10 @@ public class Main extends javax.swing.JFrame {
         );
 
         Panel.addTab("Symbol Table", jPanel2);
+
+        jScrollPane5.setViewportView(textPaneTeste);
+
+        Panel.addTab("code2", jScrollPane5);
 
         jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
@@ -151,6 +170,14 @@ public class Main extends javax.swing.JFrame {
             }
         });
         jMenu1.add(jMenuItem3);
+
+        jMenuItem6.setText("Test");
+        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem6ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem6);
 
         jMenuBar1.add(jMenu1);
 
@@ -203,7 +230,7 @@ public class Main extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(2, 2, 2)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(220, Short.MAX_VALUE))
         );
 
         pack();
@@ -211,23 +238,32 @@ public class Main extends javax.swing.JFrame {
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         file = Freader.getFile(this);
-        if(file!=null){
+        if (file != null) {
             CodeArea.setText(file.getFileText());
+        } else {
+            // codigo guardado -> para fazer highlight!
+//                excluir depois, caso nao for usar.
+//            String text = "hello world. How are you?";
+//            CodeArea.setText(text);
+//            
+//            Highlighter highlighter = CodeArea.getHighlighter();
+//
+//            HighlightPainter painter = new DefaultHighlighter.DefaultHighlightPainter(Color.pink);
+//            int p0 = text.indexOf("world");
+//            int p1 = p0 + "world".length();
+//            highlighter.addHighlight(p0, p1, painter);
+
         }
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
-    private void CodeAreaCaretUpdate(javax.swing.event.CaretEvent evt) {//GEN-FIRST:event_CodeAreaCaretUpdate
-        // TODO add your handling code here:
-    }//GEN-LAST:event_CodeAreaCaretUpdate
-
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        if (file!=null){
+        if (file != null) {
             try {
                 Freader.saveFile(file, CodeArea.getText());
             } catch (IOException ex) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }        
+        }
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
@@ -240,8 +276,7 @@ public class Main extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
-        if(file!=null)
-        {
+        if (file != null) {
             try {
                 RunResponse response = Dprocessor.runCode(file, CodeArea.getText());
                 StatisticArea.setText(response.getStatistics());
@@ -249,11 +284,64 @@ public class Main extends javax.swing.JFrame {
             } catch (IOException ex) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }else
-        {
-            JOptionPane.showMessageDialog(null,"No file opened. Try to open a file first!", "No File Opened", 0);
+        } else {
+            JOptionPane.showMessageDialog(null, "No file opened. Try to open a file first!", "No File Opened", 0);
         }
     }//GEN-LAST:event_jMenuItem5ActionPerformed
+
+    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
+
+        String markup
+              = "program Fatorial;\n" +
+                "var\n" +
+                "   N, F: integer;\n" +
+                "function Fat(N : integer) : integer;\n" +
+                "begin\n" +
+                "   if (N = 0) or (N = 1) then\n" +
+                "       begin\n" +
+                "           Fat := 1;\n" +
+                "       end\n" +
+                "   else\n" +
+                "       begin\n" +
+                "           Fat := N * Fat(N – 1);\n" +
+                "       end;\n" +
+                "end;\n\n" +
+                "begin\n" +
+                "   N := 0;\n" +
+                "   read( N );\n" +
+                "   if N >= 0 then begin\n" +
+                "       F := Fat(N);\n" +
+                "   end;\n" +
+                "   write( F );\n" +
+                "end.\n";
+
+        textPaneTeste.setText(markup);
+        // Create an AttributeSet with which to change color and font.
+        SimpleAttributeSet azulAttrs = new SimpleAttributeSet();
+        SimpleAttributeSet vermelhoAttrs = new SimpleAttributeSet();
+        SimpleAttributeSet negritoAttrs = new SimpleAttributeSet();
+
+        StyleConstants.setForeground(azulAttrs, Color.blue);
+        StyleConstants.setForeground(vermelhoAttrs, Color.red);
+        StyleConstants.setBold(negritoAttrs, true);
+        // Apply the AttributeSet to a few blocks of text.
+        
+        // setchar attr funciona assim: lugar que comeca, depois 
+        StyledDocument sdoc = textPaneTeste.getStyledDocument();
+        boolean parar = false;
+        sdoc.setCharacterAttributes(markup.indexOf("begin"), 5, azulAttrs, false);
+        sdoc.setCharacterAttributes(markup.indexOf("end"), 3, azulAttrs, false);
+        sdoc.setCharacterAttributes(40, 9, vermelhoAttrs, false);
+        sdoc.setCharacterAttributes(70, 3, vermelhoAttrs, false);
+        sdoc.setCharacterAttributes(20, 50, negritoAttrs, false);
+        
+//        sdoc.setCharacterAttributes(14, 29, vermelhoAttrs, false);
+        //JScrollPane scroll3 = new JScrollPane(textPaneTeste);
+    }//GEN-LAST:event_jMenuItem6ActionPerformed
+
+    private void CodeAreaCaretUpdate(javax.swing.event.CaretEvent evt) {//GEN-FIRST:event_CodeAreaCaretUpdate
+        // TODO add your handling code here:
+    }//GEN-LAST:event_CodeAreaCaretUpdate
 
     /**
      * @param args the command line arguments
@@ -289,6 +377,7 @@ public class Main extends javax.swing.JFrame {
                 new Main().setVisible(true);
             }
         });
+
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -307,11 +396,14 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane5;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextPane textPaneTeste;
     // End of variables declaration//GEN-END:variables
 }
